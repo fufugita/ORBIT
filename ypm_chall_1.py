652f862c572c67394a5cc1252fee8ffb6c399575
fugita <81188207+fufugita@users.noreply.github.com>
2022-10-04 05:47:00 -0300

    Update ypm_chall_1.py

--- ypm_chall_1.py
+++ ypm_chall_1.py
@@ -3,15 +3,15 @@
 # Importa bibliotecas necessárias
 import os
 import yara
-import psutil
 import time
-from subprocess import call
-from prettytable import PrettyTable
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+import psutil
+import subprocess
+import wmi
 
 # Diretório de Regras YARA
 YARA_RULES_DIR = "rules"
-
-virus = "inserir arquivo aqui"
 
 class YaraClass:
     """Caminha pelo diretório, compilando e testando regras, e escaneando arquivos
@@ -53,126 +53,87 @@ class YaraClass:
             return False
 
 
-    def scan(self, p_id):
+    def scan(self, virus):
         """Método de scan baseado nas regras compiladas
         """
         try:
             matched_rules = []
-            matches = self.rules.match(pid = p_id)
+            matches = self.rules.match(virus)
             for i in matches:
-                matched_rules.append(i)
-                print(matched_rules)
+                matched_rules.append(i)                
             return matched_rules
+           	
         except Exception as e:
             print ("'ERROR' - Scan Exception: {}".format(e))
             return 0
-            
+
+    def download(self, dir):
+        for filename in os.listdir(dir):
+            f = os.path.join(dir, filename)
+            if os.path.isfile(f):
+                self.scan(f)
+                if self.scan(f):
+                    print("Ameaça Detectada!")
+                    os.remove(f)
+                    print("Ameaça Removida!")
+
+# --------------------------------------------------------------------------------------------------------
+
+def getUserPATH():
+    global dir
+    name = os.getlogin()
+    dir = "C:\\Users\\{0}\Downloads".format(name)
+
+    return dir
+
+# --------------------------------------------------------------------------------------------------------
+
+def bait(dir):  # Cria arquivos 'bait' para o ransomware na pasta 'Downloads'
+    for n in range(1, 100):
+        with open(os.path.join(dir, f"bait {n}.txt"), "w") as arq:
+            toArq = "this is bait ignore"
+            arq.write(toArq)
+
+# --------------------------------------------------------------------------------------------------------
 
 def main():
 
     # Inicialização da YaraClass
     yara = YaraClass()
-
-    # Chamando as funções
-    # Compilando regras
     regras = yara.compile()
-
-    # Testa as regras
     yara.test_rule(regras)
 
-    # Roda um loop infinito que monitora os processos do sistema
-    while True:
+    useros = getUserPATH() 
+    bait(useros)
 
+    opt = int(input("Deseja fazer o que?\n1 - Escanear a pasta de 'Downloads'?\n2 - Verificar atividade maliciosa de algum executável?\n"))
 
-        print("==============================Process Monitor\
-        ======================================")
+    if opt == 1:
+        print("Analisando agora.\nCaso nenhuma mensagem de ameaça aparecer ou alertar que a ameaça foi removida, pode fechar tranquilamente o programa!\n")
+        yara.download(useros)
 
-        # PrettyTable para imprimir tabela no console
-        ''' t = PrettyTable(<list of headings>)
-         t.add_row(<list of cells in row>) '''
+    elif opt == 2:
 
-        # Pega informações da Rede
-        print("----Networks----")
-        table = PrettyTable(['Network', 'Status', 'Speed'])
-        for key in psutil.net_if_stats().keys():
-                name = key
-                up = "Up" if psutil.net_if_stats()[key].isup else "Down"
-                speed = psutil.net_if_stats()[key].speed
-                table.add_row([name, up, speed])
-        print(table)
-
-        # Pega informações da memória
-        print("----Memory----")
-        memory_table = PrettyTable(["Total(GB)", "Used(GB)",
-                                                                "Available(GB)", "Percentage"])
-        vm = psutil.virtual_memory()
-        memory_table.add_row([
-                f'{vm.total / 1e9:.3f}',
-                f'{vm.used / 1e9:.3f}',
-                f'{vm.available / 1e9:.3f}',
-                vm.percent
-        ])
-        print(memory_table)
-
-        # Pega os 10 processos que mais estão utilizando a CPU
-        print("----Processes----")
-        process_table = PrettyTable(['PID', 'PNAME', 'STATUS',
-                                                                'CPU', 'NUM THREADS', 'MEMORY(MB)'])
-
-        proc = []
-        # Pega os últimos PIDS (maioria processos de usuário)
-        for pid in psutil.pids()[-200:]:
-                try:
-                    p = psutil.Process(pid)
-                    print(p)
-                    scan = yara.scan(pid)
-                    print("Regras Conflitadas:", scan)
-                    ps = str(pid)
-                    print("Processo:", ps)
-                    print("\n")  
-                    # Ativa cpu_percent() pela primeira vez o que leva a retornar 0.0
-                    p.cpu_percent()
-                    proc.append(p)
-                    if len(scan) > 0 :
-                        p.kill()
-                        quit()
-
-                except Exception as e:
-                        pass
-
-        # Organiza por cpu_percent()
-        top = {}
-        time.sleep(0.1)
-        for p in proc:
-                # Ativa cpu_percent() pela segunda vez para medidas
-                top[p] = p.cpu_percent() / psutil.cpu_count()
-
-        top_list = sorted(top.items(), key=lambda x: x[1])
-        top10 = top_list[-10:]
-        top10.reverse()
-
-        '''for p, cpu_percent in top10:
-
-                # Enquanto pega processos, alguns dos subprocesses podem sair
-                # Motivo para utilizar try except
-                try:
-                        # Oneshot para melhorar a eficiência da recuperação de info 
-                        with p.oneshot():
-                                process_table.add_row([
-                                        str(p.pid),
-                                        p.name(),
-                                        p.status(),
-                                        f'{cpu_percent:.2f}' + "%",
-                                        p.num_threads(),
-                                        f'{p.memory_info().rss / 1e6:.3f}'
-                                ])
-
-                except Exception as e:
-                        pass
-        #print(process_table)'''
-
-        # Cria delay de 1 segundo
-        time.sleep(1)
-
+        c = wmi.WMI()
+        process_watcher = c.Win32_Process.watch_for("creation")
+        print("\nAnalisando agora evite de abrir outros programas.\nCaso nenhuma mensagem de ameaça aparecer ou alertar que a ameaça foi removida, pode fechar tranquilamente o programa!\n")
+        while True:
+            recent_proc = []
+            new_process = process_watcher()
+            if new_process not in recent_proc:
+                recent_proc.append(new_process.ProcessId)
+            else:
+                if new_process in recent_proc:
+                    recent_proc.remove(new_process.ProcessId)
+            for pid in reversed(recent_proc):
+                if pid != os.getpid():
+                    subprocess.run(f"taskkill /PID {pid} /F /T", shell=True)
+                    yara.download(useros)
+            recent_proc.clear()
+    
+    else:
+        print("Digite uma opção válida.")
+        
+        
 if __name__ == "__main__":
     main()
